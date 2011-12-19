7d2d1ff13ad5c77003a966325bca6e37c564771e
Sebastian Kurfuerst <sebastian@typo3.org>
2011-12-19 11:19:20 +0100

    Adding composer json file

--- Flow3ComposerConnector/FLOW3PackageInstaller.php
+++ Flow3ComposerConnector/FLOW3PackageInstaller.php
@@ -9,6 +9,22 @@ class FLOW3PackageInstaller extends \Composer\Installer\LibraryInstaller {
     public function supports($packageType)
     {
     	var_dump($packageType);
-        return ($packageType === 'FLOW3Package');
+        return ($packageType === 'flow3-package');
+    }
+
+        /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+		$extra = $package->getExtra();
+		if (!isset($extra['packageKey'])) {
+			throw new \Exception("extra[packageKey] must be set for FLOW3 packages");
+		}
+		if (!isset($extra['suggestedLocation'])) {
+			throw new \Exception("extra[suggestedLocation] must be set for FLOW3 packages");
+		}
+
+        return 'Packages/' . $extra['suggestedLocation'] . '/' . $extra['packageKey'];
     }
 }
